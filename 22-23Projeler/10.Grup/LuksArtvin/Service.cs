236276066dac74b37437dd8550fc71ab22392263
Mustafa Bulut <mustafaiste@outlook.com>
2023-05-13 20:38:45 +0300

    Servis bilgileri

---
+++ 22-23Projeler/10.Grup/LuksArtvin/Service.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+// Servis sınıfı
+public class Servis
+{
+    public int ID { get; set; } = default!;
+    public DateTime dt { get; set; } = default!;
+    public string placeOfDeparture { get; set; } = default!;
+    public string placeOfArrival { get; set; } = default!;
+    public double cost { get; set; } = default!;
+    //public Personel personel;
+    //public Arac arac;
+
+
+
+    public override string ToString()
+    {
+        return $"Date: {dt}, From: {placeOfDeparture}, To: {placeOfArrival}, Cost: {cost}";
+    }
+
+
+    
+}
